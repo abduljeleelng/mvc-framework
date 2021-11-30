23d5d4f46ac40a6223ba0f38b311b16dbdb3f867
abduljeleelng <abduljeleelng@gmail.com>
2021-11-30 11:46:33 +0100

    entring point for the app

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Load Config
+require_once 'config/config.php';
+// Autoloader
+require_once 'vendor/autoload.php';
+
+
+
+// Routes
+require_once APP_ROOT . '/routes/web.php';
+require_once APP_ROOT . '/app/Router.php';
+
+//
+echo "Hello Word";
